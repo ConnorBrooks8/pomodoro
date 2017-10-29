35ce42d23fe80dccd763dfeb20c8d9741d930f6e
Connor Brooks <ConnorBrooks8@gmail.com>
2017-10-29 15:30:36 -0400

    locks screen after block

--- pomodorocall.py
+++ pomodorocall.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+import os
+import time
 import referencetime
 import config
-import time
 elapsedtime = int(time.time()) - referencetime.startTime
 minutes = int(elapsedtime/60)
 seconds = elapsedtime % 60
 
 remainingminutes = config.pomolength - minutes
 remainingseconds = 60 - seconds
+
+if (remainingminutes <= 0) and (remainingseconds <= 0):
+    os.system('/home/connor/Documents/bash/pomo/pomodorostart.py')
+    os.system('i3lock -i /home/connor/Pictures/lockbackround.png')
 
 print(str(remainingminutes) + ':' + str(remainingseconds))
